a0bbd807248113aa13def0d62205b271294b42ab
Eris <erisapps@outlook.be>
2023-03-18 18:26:21 +0100

    Update workflows

--- build/Build.cs
+++ build/Build.cs
@@ -94,9 +94,9 @@ partial class Build : NukeBuild
 
 	Target PushToGithubPackagesRegistry => _ => _
 		.DependsOn(Pack)
-		.OnlyWhenDynamic(() => GitHubActions != null &&
+		/*.OnlyWhenDynamic(() => GitHubActions != null &&
 		                       GitHubActions.EventName == "push" &&
-		                       GitHubActions.Ref != null && GitHubActions.Ref.Contains("/refs/tags/"))
+		                       GitHubActions.Ref != null && GitHubActions.Ref.Contains("/refs/tags/"))*/
 		.Executes(() =>
 		{
 			IEnumerable<AbsolutePath> artifactPackages = ArtifactsDirectory.GlobFiles("*.nupkg");
